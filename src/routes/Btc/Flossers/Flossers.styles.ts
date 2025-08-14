837a5c6f1d21cb100fa9bc683c12445487ae9c95
Ildi Dvorani <dvoranii@ildidev.com>
2025-08-14 13:47:47 -0400

    updated copy for flossers section and changed the graphic on slide 2

--- src/routes/Btc/Flossers/Flossers.styles.ts
+++ src/routes/Btc/Flossers/Flossers.styles.ts
@@ -2,157 +2,156 @@ import styled from "styled-components";
 import { colors } from "../../../constants/colors";
 
 export const FlossersContainer = styled.div`
-   position: relative;
+  position: relative;
 `;
 
-
 interface SlideWrapperProps {
-    $gap?: string;
-    $marginLeft?: string;
+  $gap?: string;
+  $marginLeft?: string;
 }
 
 export const SlideWrapper = styled.div<SlideWrapperProps>`
-    display: flex;
-    gap: ${props => props.$gap || "2.4rem"};
-    width: 90%;
-    height: 100%;
-    ${props => props.$marginLeft && `margin-left: ${props.$marginLeft}`};
+  display: flex;
+  gap: ${(props) => props.$gap || "2.4rem"};
+  width: 90%;
+  height: 100%;
+  ${(props) => props.$marginLeft && `margin-left: ${props.$marginLeft}`};
 `;
 
 export const SlideColumn1 = styled.div`
-    width: 45%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding-top: 0rem;
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-top: 0rem;
 
-    h4 {
-      font-weight: 300;
-      font-family: "Gotham", sans-serif;
-      font-size: clamp(18px, 1.6vw, 2.4rem);
-      color: ${colors.primary};
+  h4 {
+    font-weight: 300;
+    font-family: "Gotham", sans-serif;
+    font-size: clamp(18px, 1.6vw, 2.4rem);
+    color: ${colors.primary};
 
-      span:nth-of-type(1) {
-         font-weight: 600;
-         text-transform: none;
-      }
-
-      span:nth-of-type(2) {
-         text-transform: uppercase;
-         font-weight: 400;
-      }
-
-      span:nth-of-type(3) {  
-         text-transform: uppercase; 
-         font-weight: 600;
-      }
-
-      span:nth-of-type(4) {  
-         text-transform: uppercase; 
-         font-weight: 900;
-      }
+    sup {
+      font-size: 10px;
     }
-    
+
+    span:nth-of-type(1) {
+      font-weight: 600;
+      text-transform: none;
+    }
+
+    span:nth-of-type(2) {
+      text-transform: uppercase;
+      font-weight: 400;
+    }
+
+    span:nth-of-type(3) {
+      text-transform: uppercase;
+      font-weight: 600;
+    }
+
+    span:nth-of-type(4) {
+      text-transform: uppercase;
+      font-weight: 900;
+    }
+  }
 `;
 
 export const SlideColumn2 = styled.div`
-    width: 55%; 
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
+  width: 55%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
 `;
 
 export const ImgWrapper = styled.div`
-   width: 100%;
-   position: relative;
-   border-bottom: 1px solid grey;
-   > img {
-      padding: 1rem;
-      width: 100%;
-    }
-
+  width: 100%;
+  position: relative;
+  border-bottom: 1px solid grey;
+  > img {
+    padding: 1rem;
+    width: 100%;
+  }
 `;
 
 export const ImgWrapper2 = styled.div`
-    padding-top: 1.8rem;
-    position: relative;
-    img {
-        width: 50%;
-    }
+  padding-top: 1.8rem;
+  position: relative;
+  img {
+    width: 50%;
+  }
 `;
 
 export const ListContainer = styled.div`
-    width: 92%;
-    margin-top: 12px;
+  width: 92%;
+  margin-top: 4px;
 
-    ul {
-        font-family: 'Gotham', sans-serif;
-        font-size:clamp(0.9rem, 1.2vw, 1.8rem);
-        color: ${colors.black};
-        line-height: 1.2;
-        margin-inline-start: 16px;
-        list-style: none;
+  ul {
+    font-family: "Gotham", sans-serif;
+    font-size: clamp(0.9rem, 1.2vw, 1.8rem);
+    color: ${colors.black};
+    line-height: 1;
+    margin-inline-start: 16px;
+    list-style: none;
 
-        li {
-            position: relative;
-            padding-left: 1.2rem;
-        }
+    li {
+      position: relative;
+      padding-left: 1.2rem;
+    }
 
-        li::before {
-            content: "•";
-            position: absolute;
-            left: 0;
-            font-size: 14px; 
-            color: ${colors.black};
-            vertical-align: middle;
-            line-height: 1.2; 
-        }
+    li::before {
+      content: "•";
+      position: absolute;
+      left: 0;
+      font-size: 14px;
+      color: ${colors.black};
+      vertical-align: middle;
+      line-height: 1.2;
+    }
 
-        span {
-            font-weight: 500;
-        }
+    span {
+      font-weight: 500;
+    }
 
-         sup {
-            font-size: 8px;
-         }
-   }
+    sup {
+      font-size: 8px;
+    }
+  }
 
-   @media screen and (max-width: 1080px) {
+  @media screen and (max-width: 1080px) {
     margin-top: 4px;
-   }
-
+  }
 `;
 
 interface InfoGridProps {
-    $width?: string;
+  $width?: string;
 }
 
-export const InfoGrid =  styled.div<InfoGridProps>`
-    display: grid;
-    grid-template-columns:3fr 1fr;
-    gap: 0.25rem;
-    width: ${(props) => props.$width ? props.$width : "100%"};
-    column-gap: 1rem;
+export const InfoGrid = styled.div<InfoGridProps>`
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  gap: 0.25rem;
+  width: ${(props) => (props.$width ? props.$width : "100%")};
+  column-gap: 1rem;
 
-    font-family: "Gotham", sans-serif;
-    padding-top: 0.8rem;
-    font-size: clamp(12px, 1vw, 1.4rem);
-    color: ${colors.black};
+  font-family: "Gotham", sans-serif;
+  padding-top: 0.8rem;
+  font-size: clamp(12px, 1vw, 1.4rem);
+  color: ${colors.black};
 `;
 
 export const Row = styled.div`
-    display: contents;
+  display: contents;
 `;
 
 export const Column = styled.div``;
 
 interface TitleListWrapperProps {
-    $paddingTop?: string;
+  $paddingTop?: string;
 }
 
 export const TitleListWrapper = styled.div<TitleListWrapperProps>`
-    padding-top: ${(props) => props.$paddingTop ? props.$paddingTop : "1.2rem"};
+  padding-top: ${(props) => (props.$paddingTop ? props.$paddingTop : "0.8rem")};
 `;
-
--- src/routes/Btc/Flossers/Flossers.tsx
+++ src/routes/Btc/Flossers/Flossers.tsx
@@ -3,161 +3,254 @@ import FlickityCarousel from "../../../components/FlickityCarousel/FlickityCarou
 import { BaseCarouselSlide } from "../../../styles/shared-styles";
 import Slide1Graphic from "../../../assets/btc/flosser/Slide1Graphic.webp";
 import Slide2Graphic from "../../../assets/btc/flosser/Slide2Graphic.webp";
-import Slide2GraphicSmall from "../../../assets/btc/flosser/Asset2.png"
+import Slide2GraphicSmall from "../../../assets/btc/flosser/Asset2.png";
 import ShopifyIcon from "../../../components/ShopifyIcon/ShopifyIcon";
 
-export default function BtcFlossers(){
+export default function BtcFlossers() {
+  return (
+    <S.FlossersContainer>
+      <FlickityCarousel
+        $customStyles={{
+          viewportHeight: "80vh",
+          dotPosition: {
+            bottom: "-28px",
+            right: "44px",
+          },
+        }}
+      >
+        <BaseCarouselSlide>
+          <S.SlideWrapper>
+            <S.SlideColumn1>
+              <S.TitleListWrapper>
+                <h4>
+                  <span>PORTE-FIL DENTAIRE</span>{" "}
+                  <span>
+                    Eco Clean<sup>MD</sup>
+                    <br />
+                  </span>{" "}
+                  <span>Soins écologiques</span> <span>À LA MENTHE</span>
+                </h4>
+                <S.ListContainer>
+                  <ul>
+                    <li>
+                      Le fil dentaire texturé efficace élimine 2x plus de plaque
+                      <sup>10</sup>.
+                    </li>
+                    <li>Conçu pour résister à l’effilochage ou au bris</li>
+                  </ul>
+                </S.ListContainer>
+              </S.TitleListWrapper>
+              <S.TitleListWrapper>
+                <h4>
+                  <span>PORTE-FIL DENTAIRE</span>{" "}
+                  <span>
+                    Twisted Mint<sup>MD</sup>
+                  </span>{" "}
+                  <span>Menthe torsadée</span>
+                </h4>
+                <S.ListContainer>
+                  <ul>
+                    <li>
+                      Élimine 2x plus de plaque<sup>10</sup> et couvre une
+                      surface 30% plus grande<sup>11</sup>.
+                    </li>
+                    <li>
+                      Prise souple et légère qui utilise 20% moins de plastique
+                      <sup>13</sup>.
+                    </li>
+                  </ul>
+                </S.ListContainer>
+              </S.TitleListWrapper>
+              <S.TitleListWrapper>
+                <h4>
+                  <span>PORTE-FIL DENTAIRE</span>{" "}
+                  <span>
+                    Insertion facile<sup>MD</sup>
+                  </span>{" "}
+                  <span>À LA MENTHE</span>
+                </h4>
+                <S.ListContainer>
+                  <ul>
+                    <li>
+                      Fil dentaire soyeux et non collant à la menthe qui glisse
+                      facilement entre les zones de contact les plus étroites.
+                    </li>
+                    <li>
+                      Prise souple et légère qui utilise 20% moins de plastique
+                      <sup>13</sup>.
+                    </li>
+                  </ul>
+                </S.ListContainer>
+              </S.TitleListWrapper>
+              <S.TitleListWrapper>
+                <h4>
+                  <span>PORTE-FIL DENTAIRE</span>{" "}
+                  <span>
+                    Soins intenses<sup>MD</sup>
+                  </span>{" "}
+                  <span>À LA MENTHE</span>
+                </h4>
+                <S.ListContainer>
+                  <ul>
+                    <li>
+                      Fil dentaire durable enduit de vitamine E et de fluorure.
+                    </li>
+                    <li>
+                      Conçu pour résister à l'effilochage et au bris
+                      <sup>12</sup>.
+                    </li>
+                    <li>
+                      Prise souple et légère qui utilise 20% moins de plastique
+                      <sup>13</sup>.
+                    </li>
+                  </ul>
+                </S.ListContainer>
+              </S.TitleListWrapper>
+              <S.TitleListWrapper>
+                <h4>
+                  <span>PORTE-FIL DENTAIRE</span>{" "}
+                  <span>
+                    Soins professionnels<sup>MD</sup>
+                  </span>{" "}
+                  <span>À LA MENTHE</span>
+                </h4>
+                <S.ListContainer>
+                  <ul>
+                    <li>
+                      Le fil dentaire texturé efficace élimine 2x plus de plaque
+                      <sup>10</sup>.
+                    </li>
+                    <li>
+                      Conçu pour résister à l'effilochage ou au bris
+                      <sup>12</sup>.
+                    </li>
+                    <li>
+                      Prise souple et légère qui utilise 20% moins de plastique
+                      <sup>13</sup>.
+                    </li>
+                  </ul>
+                </S.ListContainer>
+              </S.TitleListWrapper>
+            </S.SlideColumn1>
+            <S.SlideColumn2>
+              <S.ImgWrapper>
+                <img src={Slide1Graphic} />
+                <ShopifyIcon
+                  href="https://www.shopify.com/ca"
+                  absolute
+                  width="30px"
+                  bottom="35px"
+                  right="38px"
+                />
+              </S.ImgWrapper>
 
-    return(
-        <S.FlossersContainer>
-            <FlickityCarousel 
-                $customStyles={{
-                    viewportHeight: '80vh',
-                    dotPosition: {
-                    bottom: '-28px',
-                    right: '44px'
-                    }
-                }}
-            >
+              <S.InfoGrid>
+                <S.Row>
+                  <S.Column>831 Eco Clean™ Flosser Mint</S.Column>
+                  <S.Column>48 packs of 2</S.Column>
+                </S.Row>
+                <S.Row>
+                  <S.Column>828 Twisted Mint™ Flosser</S.Column>
+                  <S.Column>48 packs of 3</S.Column>
+                </S.Row>
+                <S.Row>
+                  <S.Column>887 Comfort Slide Flossers Mint</S.Column>
+                  <S.Column>48 packs of 2</S.Column>
+                </S.Row>
+                <S.Row>
+                  <S.Column>
+                    888 Advanced Care Flossers Mint + Vit E & Fluoride
+                  </S.Column>
+                  <S.Column>48 packs of 3</S.Column>
+                </S.Row>
+                <S.Row>
+                  <S.Column>891 Professional Clean Flossers Mint</S.Column>
+                  <S.Column>48 packs of 3</S.Column>
+                </S.Row>
+              </S.InfoGrid>
+            </S.SlideColumn2>
+          </S.SlideWrapper>
+        </BaseCarouselSlide>
 
-             <BaseCarouselSlide>
-                <S.SlideWrapper>
-                    <S.SlideColumn1>
-                        <S.TitleListWrapper>
-                            <h4><span>Eco</span> <span>Clean®</span> <span>Flossers</span> <span>Mint</span></h4>
-                            <S.ListContainer>
-                                <ul>
-                                    <li>Textured floss effectively removes 2x more plaque<sup>10</sup> and is designed to resist shredding or breaking<sup>12</sup>.</li>
-                                    <li>Lightweight handle is crafted from 70% plant-based materials.</li>                     
-                                </ul>  
-                            </S.ListContainer>
-                        </S.TitleListWrapper>
-                        <S.TitleListWrapper>
-                        <h4><span>Twisted</span> <span>Mint™</span> <span>Flossers</span></h4>
-                         <S.ListContainer>
-                            <ul>
-                                <li>Twisted mint floss removes 2x more plaque<sup>10</sup> and covers 30% more surface area<sup>11</sup>.</li>
-                                <li>Lightweight flexible grip uses 20% less plastic<sup>13</sup>.</li>
-                                
-                            </ul>  
-                        </S.ListContainer>
-                        </S.TitleListWrapper>
-                        <S.TitleListWrapper>
-                        <h4><span>Comfort</span> <span>Slide</span> <span>Flossers</span> <span>Mint</span></h4>
-                        <S.ListContainer>
-                            <ul>
-                                <li>Silky, non-stick mint floss easily slides between the tightest contacts for gentle and effective flossing.</li>
-                                <li>Lightweight flexible grip uses 20% less plastic<sup>13</sup>.</li>
-                            </ul>  
-                        </S.ListContainer>
-                        </S.TitleListWrapper>
-                        <S.TitleListWrapper>
-                        <h4><span>Advanced</span> <span>Care</span> <span>Flossers</span> <span>Mint</span></h4>
-                        <S.ListContainer>
-                            <ul>
-                                <li>Durable floss coated with Vitamin E & Fluoride.</li>
-                                <li>Designed to resist shredding or breaking<sup>12</sup>.</li>
-                                <li>Lightweight flexible grip uses 20% less plastic<sup>13</sup>.</li>                  
-                                <li>20% LESS PLASTIC<sup>13</sup> mint flavour on all flossers.</li>                  
-                            </ul>  
-                        </S.ListContainer>
-                        </S.TitleListWrapper>
-                         <S.TitleListWrapper>
-                        <h4><span>Professional</span> <span>Clean®</span> <span>Flossers</span> <span>Mint</span></h4>
-                        <S.ListContainer>
-                            <ul>
-                                <li>Textured floss effectively removes 2x more plaque<sup>10</sup> and is designed to resist shredding or breaking<sup>12</sup>.</li>
-                                <li>Lightweight flexible grip uses 20% less plastic<sup>13</sup>.</li>                  
-                            </ul>  
-                        </S.ListContainer>
-                        </S.TitleListWrapper>
-                    </S.SlideColumn1>
-                    <S.SlideColumn2>
-                        <S.ImgWrapper>
-                            <img src={Slide1Graphic}/> 
-                            <ShopifyIcon href="https://www.shopify.com/ca" absolute width="30px" bottom="35px" right="38px"/>   
-                        </S.ImgWrapper>
-                        
-                            <S.InfoGrid>
-                                <S.Row>                            
-                                    <S.Column>831 Eco Clean™ Flosser Mint</S.Column>
-                                    <S.Column>48 packs of 2</S.Column>
-                                </S.Row>
-                                <S.Row>
-                                    <S.Column>828 Twisted Mint™ Flosser</S.Column>
-                                    <S.Column>48 packs of 3</S.Column>
-                                </S.Row>
-                                <S.Row>
-                                    <S.Column>887 Comfort Slide Flossers Mint</S.Column>
-                                    <S.Column>48 packs of 2</S.Column>
-                                </S.Row>
-                                <S.Row>
-                                    <S.Column>888 Advanced Care Flossers Mint + Vit E & Fluoride</S.Column>
-                                    <S.Column>48 packs of 3</S.Column>
-                                </S.Row>
-                                <S.Row>
-                                    <S.Column>891 Professional Clean Flossers Mint</S.Column>
-                                    <S.Column>48 packs of 3</S.Column>
-                                </S.Row>
-                            </S.InfoGrid>
-                    </S.SlideColumn2>
+        {/* Slide 2 */}
 
-                    </S.SlideWrapper>
-             </BaseCarouselSlide>
+        <BaseCarouselSlide>
+          <S.SlideWrapper $marginLeft="24px" $gap="0.3rem">
+            <S.SlideColumn1>
+              <S.TitleListWrapper $paddingTop="0">
+                <h4>
+                  <span>PORTE-FIL DENTAIRE</span>{" "}
+                  <span>
+                    Soins professionnels plus<sup>MD</sup>
+                  </span>{" "}
+                  <span>À LA MENTHE</span>
+                </h4>
+                <S.ListContainer>
+                  <ul>
+                    <li>
+                      Embout en caoutchouc spécialement conçu pour masser les
+                      gencives tout en éliminant délicatement les débris.
+                    </li>
+                    <li>
+                      Fil dentaire durable conçu pour glisser facilement entre
+                      les dents et résister à l'effilochage ou au bris
+                      <sup>12</sup>.
+                    </li>
+                  </ul>
+                </S.ListContainer>
+              </S.TitleListWrapper>
 
-            {/* Slide 2 */}
+              <S.TitleListWrapper $paddingTop="1.2rem">
+                <h4>
+                  <span>PORTE-FIL DENTAIRE</span>{" "}
+                  <span>
+                    Dual Technique<sup>MD</sup>
+                  </span>
+                  <br /> <span>À LA MENTHE</span>
+                </h4>
+                <S.ListContainer>
+                  <ul>
+                    <li>
+                      Deux têtes conçues pour accéder aux dents avant et
+                      arrière.
+                    </li>
+                    <li>
+                      Fil dentaire torsadé à la menthe sur la tête à angle qui
+                      élimine 2x plus de plaque<sup>10</sup> et couvre une
+                      surface 30% plus grande<sup>11</sup>.
+                    </li>
+                  </ul>
+                </S.ListContainer>
+              </S.TitleListWrapper>
 
-             <BaseCarouselSlide>
-                <S.SlideWrapper $marginLeft="24px" $gap="0.3rem">
-                    <S.SlideColumn1>
-                        <S.TitleListWrapper $paddingTop="0">
-                            <h4><span>Professional</span> <span>Clean Plus</span><br/> <span>Flosser Picks</span> <span>Mint</span></h4>
-                            <S.ListContainer>
-                                <ul>
-                                    <li>Specialy designed rubber pick massages gums while gently removing debris</li>
-                                    <li>Strong floss designed to slide easily between teeth and resist shredding or breaking</li>
-                                    
-                                </ul>  
-                            </S.ListContainer>
-                        </S.TitleListWrapper>
-                     
-                        <S.TitleListWrapper $paddingTop="1.2rem">
-                            <h4><span>Dual</span> <span>Technique®</span><br/> <span>Flossers</span> <span>Mint</span></h4>
-                            <S.ListContainer>
-                                <ul>
-                                    <li>Dual ends specifically designed to reach front and back teeth</li>
-                                    <li>Twisted mint floss on the angled side removes 2x more plaque<sup>2</sup> and covers 30% more surface area<sup>3</sup></li>
-                                    
-                                </ul>  
-                            </S.ListContainer>
-                        </S.TitleListWrapper>
-
-                        <S.ImgWrapper2>
-                            <img src={Slide2GraphicSmall}/>
-                            
-                        </S.ImgWrapper2>
-
-                    </S.SlideColumn1>
-                    <S.SlideColumn2>
-                        <S.ImgWrapper>
-                            <img src={Slide2Graphic}/>
-                        </S.ImgWrapper>
-                         <S.InfoGrid $width="90%">
-                                <S.Row>                            
-                                    <S.Column>885 Professional Clean PLUS Flosser Mint</S.Column>
-                                    <S.Column>48 packs of 2</S.Column>
-                                </S.Row>
-                                <S.Row>
-                                    <S.Column>821 Dual Technique® Flossers Mint</S.Column>
-                                    <S.Column>48 packs of 3</S.Column>
-                                </S.Row>
-                                <ShopifyIcon href="https://www.shopify.com/ca" width="30px" marginTop="4px"/>
-                        </S.InfoGrid>
-                    </S.SlideColumn2>
-                    </S.SlideWrapper>
-             </BaseCarouselSlide>
-            </FlickityCarousel>
-            {/* <ShopifyIcon/> */}
-        </S.FlossersContainer>
-    )
+              <S.ImgWrapper2>
+                <img src={Slide2GraphicSmall} />
+              </S.ImgWrapper2>
+            </S.SlideColumn1>
+            <S.SlideColumn2>
+              <S.ImgWrapper>
+                <img src={Slide2Graphic} />
+              </S.ImgWrapper>
+              <S.InfoGrid $width="90%">
+                <S.Row>
+                  <S.Column>885 Professional Clean PLUS Flosser Mint</S.Column>
+                  <S.Column>48 packs of 2</S.Column>
+                </S.Row>
+                <S.Row>
+                  <S.Column>821 Dual Technique® Flossers Mint</S.Column>
+                  <S.Column>48 packs of 3</S.Column>
+                </S.Row>
+                <ShopifyIcon
+                  href="https://www.shopify.com/ca"
+                  width="30px"
+                  marginTop="4px"
+                />
+              </S.InfoGrid>
+            </S.SlideColumn2>
+          </S.SlideWrapper>
+        </BaseCarouselSlide>
+      </FlickityCarousel>
+      {/* <ShopifyIcon/> */}
+    </S.FlossersContainer>
+  );
 }
